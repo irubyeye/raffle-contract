a0e7e84a1c4aa8a08458b4981dc168b82aaecd0c
irubyeye <irubyeye@gmail.com>
2024-01-23 01:31:58 +0200

    fix: imports

--- contracts/RandomNumberConsumerV2.sol
+++ contracts/RandomNumberConsumerV2.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
-import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "../node_modules/@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "../node_modules/@chainlink/contracts/src/v0.8/vrf/VRFConsumerBaseV2.sol";
 
 contract RandomNumberConsumerV2 is VRFConsumerBaseV2 {
     VRFCoordinatorV2Interface immutable COORDINATOR;
