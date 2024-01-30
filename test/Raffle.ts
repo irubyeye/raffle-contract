92652b5112d371879549cf3b8ab5000d8d77dd75
irubyeye <irubyeye@gmail.com>
2024-01-30 13:52:49 +0200

    fix: null address

--- test/Raffle.ts
+++ test/Raffle.ts
@@ -105,7 +105,7 @@ describe("Advanced voting system", async () => {
       "Raffle"
     );
     raffleContract = await RaffleFactory.deploy(
-      randomNumberConsumerAddress,
+      "0x000000000000000000000000000000000000dead",
       "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
       "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D"
     );
